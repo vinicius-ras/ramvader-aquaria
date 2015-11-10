cbe51438ea6879edbf188af37ca21f30bf13590b
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2015-11-10 18:20:37 +0000

    * Created the "AquariaTrainerCSharp" project. This project is a recreation of a former project ("Aquaria Trainer") which has been developed in C++ language years ago, and will now be recreated in C# to provide better maintanability. The purpose of this project is to create a desktop application which runs on Windows platforms and enables cheats for the indie game "Aquaria".

---
+++ AquariaTrainerCSharp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AquariaTrainerCSharp
+{
+    /// <summary>
+    /// Implements the logic behind the trainer's MainWindow.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
