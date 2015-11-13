f7e3e13c90ca23e9908477fc03987cc6309d6cbc
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2015-11-13 18:56:20 +0000

    * Implemented the logic which synchronizes the values of variables in the trainer with the variables that are injected into the game's memory space.

--- AquariaTrainerCSharp/MainWindow.xaml.cs
+++ AquariaTrainerCSharp/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Diagnostics;
-using System.Windows;
-using System.Linq;
-using RAMvader;
+﻿using RAMvader;
 using RAMvader.CodeInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 
 namespace AquariaTrainerCSharp
 {
@@ -15,6 +17,28 @@ namespace AquariaTrainerCSharp
         #region PRIVATE CONSTANTS
         /// <summary>The name of the process which runs the game.</summary>
         private const string GAME_PROCESS_NAME = "Aquaria";
+        #endregion
+
+
+
+
+
+        #region PRIVATE STATIC FIELDS
+        /// <summary>Maps a DependencyProperty to its associated injected variable.</summary>
+        private static Dictionary<DependencyProperty,EVariable> sm_dependencyPropertyToInjectedVariable = new Dictionary<DependencyProperty, EVariable>();
+        #endregion
+
+
+
+
+
+        #region DEPENDENCY PROPERTIES
+        /// <summary>DependencyProperty associated with the value of the "#EVariable.evVarOverrideVelocity" injected variable.</summary>
+        public static readonly DependencyProperty PlayerVelocityFactorProperty = RegisterInjectedVariableDependencyProperty(
+            "PlayerVelocityFactor", EVariable.evVarOverrideVelocity );
+        /// <summary>DependencyProperty associated with the value of the "#EVariable.evVarOverrideEnergyShotDamage" injected variable.</summary>
+        public static readonly DependencyProperty EnergyShotDamageFactorProperty = RegisterInjectedVariableDependencyProperty(
+            "EnergyShotDamageFactor", EVariable.evVarOverrideEnergyShotDamage );
         #endregion
 
 
@@ -33,14 +57,57 @@ namespace AquariaTrainerCSharp
 
 
 
-        #region PRIVATE CALLBACKS
+        #region PRIVATE STATIC METHODS
+        /// <summary>
+        /// Registers a DependencyProperty which is associated to a given injected variable of the trainer.
+        /// The default value and type for the property is retrieved from the associated injected variable.
+        /// </summary>
+        /// <param name="propertyName">The name of the DependencyProperty to be registered.</param>
+        /// <param name="varID">The injected variable to be associated with the DependencyProperty.</param>
+        /// <returns>Returns a DependencyProperty representing the injected variable.</returns>
+        private static DependencyProperty RegisterInjectedVariableDependencyProperty( string propertyName, EVariable varID )
+        {
+            // Retrieve information about the variable
+            MemberInfo enumeratorInfo = typeof( EVariable ).GetField( varID.ToString() );
+            VariableDefinitionAttribute varDef = enumeratorInfo.GetCustomAttribute<VariableDefinitionAttribute>();
+            Object defaultVarValue = varDef.InitialValue;
+            Type varType = varDef.InitialValue.GetType();
+
+            // Register the DependencyProperty
+            DependencyProperty newDepProperty = DependencyProperty.Register( propertyName, varType, typeof( MainWindow ),
+                new PropertyMetadata( defaultVarValue, InjectedVariableValueChanged ) );
+            sm_dependencyPropertyToInjectedVariable.Add( newDepProperty, varID );
+            return newDepProperty;
+        }
+        #endregion
+
+
+
+
+
+        #region PRIVATE METHODS
         /// <summary>Called when the trainer needs to be detached from the game's process.</summary>
         private void detachFromGame()
         {
+            // Detach from the target process
             if ( GameMemoryIO.IsAttached() )
             {
                 GameMemoryInjector.ResetAllocatedMemoryData();
                 GameMemoryIO.DetachFromProcess();
+            }
+
+            // Reset all of the values of the DependencyProperty objects which represent injected variables
+            foreach ( KeyValuePair<DependencyProperty, EVariable> curPair in sm_dependencyPropertyToInjectedVariable )
+            {
+                // Retrieve the initial value of the injected variable
+                DependencyProperty varDepProp = curPair.Key;
+                EVariable varID = curPair.Value;
+
+                FieldInfo enumeratorInfo = typeof( EVariable ).GetField( varID.ToString() );
+                VariableDefinitionAttribute varDef = enumeratorInfo.GetCustomAttribute<VariableDefinitionAttribute>();
+                
+                // Reset the variable back to its initial value
+                this.SetValue( varDepProp, varDef.InitialValue );
             }
         }
         #endregion
@@ -72,6 +139,11 @@ namespace AquariaTrainerCSharp
 
 
         #region EVENT CALLBACKS
+        /// <summary>
+        /// Called when the user clicks the "'Attach to' (or 'Detach from') game" button.
+        /// </summary>
+        /// <param name="sender">Object which sent the event.</param>
+        /// <param name="e">Arguments from the event.</param>
         private void ButtonClickAttachToGame( object sender, RoutedEventArgs e )
         {
             // Is the trainer currently attached to the game's process?
@@ -85,7 +157,10 @@ namespace AquariaTrainerCSharp
                 {
                     // Try to attach to the game's process
                     if ( GameMemoryIO.AttachToProcess( gameProcess ) )
+                    {
+                        // Inject the trainer's code and variables into the game's memory!
                         GameMemoryInjector.Inject();
+                    }
                     else
                         MessageBox.Show( this,
                             Properties.Resources.strMsgFailedToAttach, Properties.Resources.strMsgFailedToAttachCaption,
@@ -95,6 +170,29 @@ namespace AquariaTrainerCSharp
                     MessageBox.Show( this,
                         Properties.Resources.strMsgGamesProcessNotFound, Properties.Resources.strMsgGamesProcessNotFoundCaption,
                         MessageBoxButton.OK, MessageBoxImage.Exclamation );
+            }
+        }
+
+
+        /// <summary>
+        /// Called when one of the DependencyProperty objects from the #MainWindow has its value changed.
+        /// This method automatically updates the value of the corresponding injected variable into the game's process' memory space.
+        /// </summary>
+        /// <param name="depObj">A reference to the DependencyObject which owns the changed DependencyProperty. This parameter is
+        /// effectivelly a reference to the #MainWindow object.</param>
+        /// <param name="args">Contains data related to the event which changed the DependencyProperty's value.</param>
+        private static void InjectedVariableValueChanged( DependencyObject depObj, DependencyPropertyChangedEventArgs args )
+        {
+            // If the trainer is attached to the game, update the value of the variable into the game's memory space
+            MainWindow mainWnd = (MainWindow) App.Current.MainWindow;
+            if ( mainWnd.GameMemoryIO.Attached )
+            {
+                // Retrieve the information we need to update the game's memory...
+                EVariable varID = sm_dependencyPropertyToInjectedVariable[args.Property];
+                IntPtr varAddressInGame = mainWnd.GameMemoryInjector.GetInjectedVariableAddress( varID );
+
+                // Update the game's memory
+                mainWnd.GameMemoryIO.WriteToTarget( varAddressInGame, args.NewValue );
             }
         }
         #endregion
